1bb38c98e4bede4007ef2783dcd7b5d40b45a1bb
shreyas2001 <67593308+shreyas2001@users.noreply.github.com>
2025-05-23 20:42:36 +0530

    Update tooljet.cy.js

--- tooljet.cy.js
+++ tooljet.cy.js
@@ -23,7 +23,7 @@ describe("ToolJet pplication Verifcation",()=>{
 
 
         cy.get("button[class='create-new-app-button col-11  btn btn-primary']").click()
-        cy.get("input[placeholder='Enter app name']").type("CREATis")
+        cy.get("input[placeholder='Enter app name']").type("TRIAL98")
 
         Cypress.on('uncaught:exception', (err, runnable) => {
         // Allow known issues to pass without failing the test
@@ -39,7 +39,7 @@ describe("ToolJet pplication Verifcation",()=>{
         // create aapp
         cy.get("button[form='createAppForm']").click()
         for(let i=0;i<5;i++){
-            cy.get("span > button[class='driver-next-btn']").click()
+            cy.get("button[class='driver-next-btn']").click()
         }
         cy.get("p[class='widgets-manager-header']").contains('Components')
         //validating in above line 
@@ -53,14 +53,15 @@ describe("ToolJet pplication Verifcation",()=>{
         //on to update           div[data-cy="aoc-card"]
         cy.get('button.tj-primary-btn.tj-text-xsm.edit-button').first().click({ force: true });
         //to rename
-        cy.get('div[class="p-0 m-0 d-flex align-items-center"] > div >div > input[class="form-control-plaintext form-control-plaintext-sm  "]').type('add', { force: true });
+        cy.wait(7000);
+        cy.get("#app > div > div.main-wrapper > div.wrapper.editor > div.header > header > div > div > div.header-inner-wrapper.d-flex > div.flex-grow-1.d-flex.align-items-center > div.p-0.m-0.d-flex.align-items-center > div.global-settings-app-wrapper.p-0.m-0 > div > input").type('add', { force: true });
         //back to home page
         cy.get("div[class='cursor-pointer']").click()
         cy.get("a[data-cy='back-to-app-option']").click()
 
         // delete
         cy.get('.app-list .home-app-card-header').eq(1).find('svg').first().click()
-        cy.get('div[class="field mb-3 field__danger"]').click()
-        cy.get('button[data-cy="yes-button"]').should("be.enabled").click()
+        cy.get('#app > div > div.main-wrapper > div.row.m-auto > div.col > div > div > div > div.col.home-page-content.bg-light-gray > div.w-100.mb-5.container.home-page-content-container > div.app-list > div > div > div:nth-child(1) > div > div > div.row.home-app-card-header > div > div:nth-child(2) > div > svg > path').click()
+        cy.get('div[role="tooltip"] > div > div > div[class="field mb-3 field__danger"] > span').should("be.enabled").click()
     })
 })
